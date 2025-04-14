973bae7efdfd6ff53f87574f6d8fc3411b77a24a
fjmasis <102482854+fjmasis@users.noreply.github.com>
2025-04-14 17:20:45 -0600

    conexion

--- Program.cs
+++ Program.cs
@@ -33,15 +33,16 @@ builder.Services.AddScoped<IPerfumeService, PerfumeService>();
 // Add CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
 });
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
