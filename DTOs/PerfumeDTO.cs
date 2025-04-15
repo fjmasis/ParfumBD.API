a60bd57e975fb3e266782bdf62b6f82aaf406c9a
fjmasis <102482854+fjmasis@users.noreply.github.com>
2025-04-14 19:31:59 -0600

    modifique el api para agregar imagenes en el put

--- DTOs/PerfumeDTO.cs
+++ DTOs/PerfumeDTO.cs
@@ -28,7 +28,10 @@
     {
         public string Nombre { get; set; } = string.Empty;
         public string Marca { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string TipoFragancia { get; set; } = string.Empty;
         public decimal Precio { get; set; }
+        public string Imagen { get; set; } = string.Empty;
         public int Stock { get; set; }
         public bool Estado { get; set; }
     }
